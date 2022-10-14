d731dc9bb36f960a3badbdefe642867ad0c7f7a6
Benji4C <thegreatben27@gmail.com>
2022-10-14 20:43:26 +0100

    corrected a few errorsl

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Request, Response } from 'express';
 import { v4 as uuidv4 } from 'uuid';
 import { UserInstance } from '../models/users';
